6d7bc533fd0db0c2be18dba5124f327f49eabee6
Walter Martin <xctrunner@gmail.com>
2016-11-26 20:43:57 -0500

    provisional work on vectorization

--- chess_agents.py
+++ chess_agents.py
@@ -5,116 +5,116 @@ import time
 
 class Agent:
 
-	def __init__(self, eval_func, color=chess.WHITE, depth='1'):
-		self.color = color
-		self.eval_func = eval_func
-		self.depth = int(depth)
+    def __init__(self, eval_func, color=chess.WHITE, depth='1'):
+        self.color = color
+        self.eval_func = eval_func
+        self.depth = int(depth)
 
-	def get_move(self, game_state):
-		raise Exception("Undefined!")
+    def get_move(self, game_state):
+        raise Exception("Undefined!")
 
 
 class RandomAgent(Agent):
-	def get_move(self, game_state):
-		moves = game_state.get_legal_moves()
-		move = random.sample(moves, 1)[0]
-		return move
-		
+    def get_move(self, game_state):
+        moves = game_state.get_legal_moves()
+        move = random.sample(moves, 1)[0]
+        return move
+
 
 class AlphaBetaAgent(Agent):
-	def get_move(self, game_state):
-		"""
-		Return minimax move using self.depth, self.eval_func, and alpha-beta pruning.
-		"""
+    def get_move(self, game_state):
+        """
+        Return minimax move using self.depth, self.eval_func, and alpha-beta pruning.
+        """
 
-		moves = game_state.get_legal_moves()
+        moves = game_state.get_legal_moves()
 
-		if len(moves) == 0:
-			return None
-
-
-		#p = .1		# with probability p, choose random action
-		#if random.uniform(0,1) < p:
-		#	return random.sample(moves, 1)[0]
-
-		values = {}
-		alpha = -99999
-		for move in moves:
-			values[move] = self._alpha_beta_value(move, game_state, -99999, 99999, 0, self.color)
-			alpha = max(alpha, values[move])
-
-		# return action with max utility, 
-		# random action if there's a tie
-		best_val = max(values.values())	
-		best_actions = []
-		for k, v in values.iteritems():
-			if v == best_val:
-				best_actions.append(k)
-		return random.sample(best_actions, 1)[0]
+        if len(moves) == 0:
+            return None
 
 
+        #p = .1		# with probability p, choose random action
+        #if random.uniform(0,1) < p:
+        #	return random.sample(moves, 1)[0]
 
-	def _alpha_beta_value(self, move, game_state, alpha, beta, depth, color):
-		"""
-		Helper function for performing alpha-beta pruning.
-		"""
-		# has max depth been reached?
-		if depth == self.depth:
-			return self.eval_func(game_state, color)
+        values = {}
+        alpha = -99999
+        for move in moves:
+            values[move] = self._alpha_beta_value(move, game_state, -99999, 99999, 0, self.color)
+            alpha = max(alpha, values[move])
 
-		# get next game state
-		next_state = game_state.generate_successor(move)
+        # return action with max utility,
+        # random action if there's a tie
+        best_val = max(values.values())
+        best_actions = []
+        for k, v in values.iteritems():
+            if v == best_val:
+                best_actions.append(k)
+        return random.sample(best_actions, 1)[0]
 
-		# has agent won?
-		if next_state.is_game_over():
-			return 99999
 
-		# does agent move next?
-		next_color = not color
 
-		# get information about next state
-		next_moves = next_state.get_legal_moves()
+    def _alpha_beta_value(self, move, game_state, alpha, beta, depth, color):
+        """
+        Helper function for performing alpha-beta pruning.
+        """
+        # has max depth been reached?
+        if depth == self.depth:
+            return self.eval_func(game_state, color)
 
-		# if this agent is to move
-		if next_color == self.color:
+        # get next game state
+        next_state = game_state.generate_successor(move)
 
-			# increment depth
-			depth += 1
+        # has agent won?
+        if next_state.is_game_over():
+            return 99999
 
-			# if we've reached a terminal state
-			# update alpha and return terminal value
-			if next_moves == []:
-				term_val = self.eval_func(next_state, next_color)
-				alpha = max(alpha, term_val)
-				return term_val
+        # does agent move next?
+        next_color = not color
 
-			# find next action with max utility
-			v = -99999
-			for mv in next_moves:
-				mvValue = self._alpha_beta_value(mv, next_state, alpha, beta, depth, next_color)
-				v = max(v, mvValue)
-				# prune if value is great enough
-			if v > beta:
-			  return v
-			alpha = max(alpha, v)
-			return v
+        # get information about next state
+        next_moves = next_state.get_legal_moves()
 
-		# if opponent is to move
-		else:
-			# if we've reached a terminal state
-			# return terminal value without updating alpha/beta
-			if next_moves == []:
-				term_val = self.eval_func(next_state, next_color)
-				return term_val
+        # if this agent is to move
+        if next_color == self.color:
 
-			# find next action with min utility
-			v = 99999
-			for mv in next_moves:
-				mvValue = self._alpha_beta_value(mv, next_state, alpha, beta, depth, next_color)
-				v = min(v, mvValue)
-				#prune if value is small enough
-				if v < alpha:
-					return v
-				beta = min(beta, v)
-			return v
+            # increment depth
+            depth += 1
+
+            # if we've reached a terminal state
+            # update alpha and return terminal value
+            if next_moves == []:
+                term_val = self.eval_func(next_state, next_color)
+                alpha = max(alpha, term_val)
+                return term_val
+
+            # find next action with max utility
+            v = -99999
+            for mv in next_moves:
+                mvValue = self._alpha_beta_value(mv, next_state, alpha, beta, depth, next_color)
+                v = max(v, mvValue)
+                # prune if value is great enough
+            if v > beta:
+              return v
+            alpha = max(alpha, v)
+            return v
+
+        # if opponent is to move
+        else:
+            # if we've reached a terminal state
+            # return terminal value without updating alpha/beta
+            if next_moves == []:
+                term_val = self.eval_func(next_state, next_color)
+                return term_val
+
+            # find next action with min utility
+            v = 99999
+            for mv in next_moves:
+                mvValue = self._alpha_beta_value(mv, next_state, alpha, beta, depth, next_color)
+                v = min(v, mvValue)
+                #prune if value is small enough
+                if v < alpha:
+                    return v
+                beta = min(beta, v)
+            return v
 
--- game.py
+++ game.py
@@ -16,37 +16,36 @@ board = losing_board.LosingBoard(no_kings=True)
 a1 = chess_agents.AlphaBetaAgent(color=chess.WHITE, eval_func=evaluation.weighted_piece_count_w_captures, depth='1')
 a2 = chess_agents.RandomAgent(color=chess.BLACK, eval_func=evaluation.weighted_piece_count_w_captures, depth='1')
 
-
 while True:
 
-	outer_break = False
-	turn = False
-	for agent in [a1,a2]:
-		mv = agent.get_move(board)
-		if not mv:
-			outer_break = True
-			print "It's a draw."
-			break
-		board.move(mv)
+    outer_break = False
+    turn = False
+    for agent in [a1,a2]:
+        mv = agent.get_move(board)
+        if not mv:
+            outer_break = True
+            print "It's a draw."
+            break
+        board.move(mv)
 
 
-		print "Agent " + str(turn + 1) + " makes move: "+ str(mv)
-		print board
-		print
+        print "Agent " + str(turn + 1) + " makes move: "+ str(mv)
+        print board
+        print
 
-		turn = not turn
+        turn = not turn
 
-		if board.is_game_over():
-			print 
-			print "Agent " + str(turn + 1) + " victorious in " + str(board.board.fullmove_number) + " plies."
-			print
-			outer_break = True
-			break
-		if board.is_draw():
-			print "It's a draw in " + str(board.board.fullmove_number) + " plies."
-			print
-			draw = True
-			break
-	
-	if outer_break: break
+        if board.is_game_over():
+            print
+            print "Agent " + str(turn + 1) + " victorious in " + str(board.board.fullmove_number) + " plies."
+            print
+            outer_break = True
+            break
+        if board.is_draw():
+            print "It's a draw in " + str(board.board.fullmove_number) + " plies."
+            print
+            draw = True
+            break
+
+    if outer_break: break
 
