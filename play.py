1c7027e2d274e92880271ab36f7ecdaca5821ea1
ryanwallace96 <ryanwallace@college.harvard.edu>
2016-12-04 14:56:07 -0500

    Adding controllers.

--- play.py
+++ play.py
@@ -6,7 +6,7 @@ import softmax
 import multilayer
 import td_lambda
 
-def play_game(agent_1, eval_func_1, depth_1, agent_2, eval_func_2, depth_2, td_parameters, softmax_parameters, multilayer_parameters):
+def play_game(agent_1, eval_func_1, depth_1, agent_2, eval_func_2, depth_2, td_parameters, softmax_parameters, multilayer_parameters, board=losing_board.LosingBoard(no_kings=False)):
     # agent 1 evaluator
     if eval_func_1 == evaluation.SoftmaxEval:
         model = softmax.Softmax(*softmax_parameters)
@@ -51,11 +51,10 @@ def play_game(agent_1, eval_func_1, depth_1, agent_2, eval_func_2, depth_2, td_p
     else:
         evaluator_2 = eval_func_2().evaluate
 
-    # construct final agents, board, and game
+    # construct final agents, and game
     a1 = agent_1(color=chess.WHITE, eval_func=evaluator_1, depth=depth_1)
     a2 = agent_2(color=chess.BLACK, eval_func=evaluator_2, depth=depth_2)
-
-    board = losing_board.LosingBoard(no_kings=False)
+ 
     game_to_play = game.Game(board, a1, a2)
 
     # begin
